5a25f572a9597ec927efad6ab8a26c92cce205c0
Keheliya-Sathsara <keheliyasm@gmail.com>
2023-10-25 15:13:35 +0530

    created new welcome page and navigate it with start page

---
+++ lib/models/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  final Color primaryColor = const Color(0xff98B2F9);
+  final Color secondaryColor = const Color(0xff90B2F9);
+  final Color thirdColor = const Color(0xd0101010);
+}
--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/ui/welcome.dart';
+
+import '../models/constants.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Constants myConstants = Constants();
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.deepPurpleAccent, // Default colour
+        color: myConstants.primaryColor.withOpacity(.5), // Default colour
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -20,17 +24,37 @@ class GetStarted extends StatelessWidget {
             children: [
               Image.asset('assets/get-started.png'),
               const SizedBox(height: 30,),
-              Container(
-                height: 50,
-                width: size.width * 0.7,
-                decoration: const BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: const Center(
-                  child: Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Welcome()));
+                },
+                child: Container(
+                  height: 50,
+                  width: size.width * 0.7,
+                  decoration: BoxDecoration(
+                    color: myConstants.primaryColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: myConstants.thirdColor.withOpacity(0.3),
+                        blurRadius: 5, // Adjust the shadow properties as needed
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               )
+
             ],
           ),
         ),
