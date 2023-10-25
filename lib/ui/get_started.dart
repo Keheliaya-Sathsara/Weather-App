b0acf10fe1eceaada11bc4633555ba73ccab6a8f
Keheliya-Sathsara <keheliyasm@gmail.com>
2023-10-25 13:45:47 +0530

    added assets file and make changes in start page

--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -11,7 +11,29 @@ class GetStarted extends StatelessWidget {
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.deepPurpleAccent,
+        color: Colors.deepPurpleAccent, // Default colour
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Image.asset('assets/get-started.png'),
+              const SizedBox(height: 30,),
+              Container(
+                height: 50,
+                width: size.width * 0.7,
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: const Center(
+                  child: Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
